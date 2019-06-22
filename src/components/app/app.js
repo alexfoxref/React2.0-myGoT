287e4fdd2f8d506f186bb5f9ee69b411727e9f02
Alexander Demidenko <alexfoxref@gmail.com>
2019-06-22 23:50:05 +0500

    lesson 11 additional task

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import {Container} from 'reactstrap';
+import {Container, Button} from 'reactstrap';
 import Header from '../header';
 import RandomItem from '../randomItem';
 import ErrorMessage from '../errorMessage';
 import { CharactersPage, BooksPage, HousesPage, BooksItem } from '../pages';
 import gotService from '../../services/gotService';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import styled from 'styled-components';
 
 const StyledDiv = styled.div`
@@ -24,13 +24,31 @@ const StyledDiv = styled.div`
         text-align: justify;
     }
 `;
+
+const StyledNoMatch = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    button {
+        position: fixed;
+        top: 10%;
+        left: 10%;
+        transform: translate(-50%, -50%);
+        font-size: 30px;
+        color: red;
+    }
+`;
 export default class App extends Component {
 
     gotService = new gotService();
 
     state = {
         visibleRandom: false,
-        error: false
+        error: false,
+        match: true
     }
 
     componentDidCatch() {
@@ -43,6 +61,12 @@ export default class App extends Component {
         this.setState(({visibleRandom}) => ({
             visibleRandom: !visibleRandom
         }))
+    }
+
+    onMatch = () => {
+        this.setState({
+            match: false
+        });
     }
 
     render() {
@@ -91,7 +115,65 @@ export default class App extends Component {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et justo turpis. Pellentesque scelerisque gravida consequat. Phasellus in leo id diam porttitor aliquam. Ut feugiat, magna sed mollis tincidunt, mi eros consectetur orci, vitae faucibus eros augue nec arcu. Nulla facilisi. Mauris sagittis sem quis elit sagittis, at molestie velit commodo. Proin commodo ligula eget elit condimentum, a pharetra eros ornare. Duis pellentesque pharetra ligula, at hendrerit purus laoreet non. Phasellus ultrices elementum mauris a faucibus.
                 </p>
             </StyledDiv>
-        )
+        );
+
+        const dynamicBooks = (match) => {
+            const {id} = match.params;
+
+            return booksItem(id)
+        };
+
+        const routerConfig = [
+            {
+                path: '/',
+                component: () => startPage,
+                exact: true
+            },
+            {
+                path: '/characters/',
+                component: CharactersPage,
+                exact: false
+            },
+            {
+                path: '/houses/',
+                component: HousesPage,
+                exact: false
+            },
+            {
+                path: '/books/',
+                component: BooksPage,
+                exact: true
+            },
+            {
+                path: '/books/:id',
+                component: ({match}) => dynamicBooks(match),
+                exect: false
+            }
+        ];
+
+        const noMatch = (
+            <StyledNoMatch>
+                <ErrorMessage errData={{message: '404'}}/>
+                <Button onClick={this.onMatch}>
+                    <Link to="/">Main Page</Link>
+                </Button>
+            </StyledNoMatch>
+        );
+
+        const matchPath = routerConfig.filter((item) => {
+            const {path} = item;
+            return (path === window.location.pathname)
+        }).map(item => item.path);
+        
+        const noMatchPath = (matchPath.length !== 0) ? null : noMatch;
+
+        const routerItems = routerConfig.map(item => {
+            const {path, component, exact} = item;
+
+            return (
+                <Route exact={exact} path={path} component={component} key={path}/>
+            )
+        });
 
         return (
             <Router>
@@ -101,18 +183,8 @@ export default class App extends Component {
                     </Container>
                     <Container>
                         {randomChar}
-                        <Route exact path="/" component={() => startPage}/>
-                        <Route path="/characters" component={CharactersPage}/>
-                        <Route path="/houses" component={HousesPage}/>
-                        <Route exact path="/books" component={BooksPage}/>
-                        {/* Динамический путь */}
-                        <Route path="/books/:id" render={
-                            ({match}) => {
-                                const {id} = match.params;
-
-                                return booksItem(id)
-                            }
-                        }/>
+                        {routerItems}
+                        <Route component={() => noMatchPath}/>
                     </Container>
                 </div>
             </Router>
